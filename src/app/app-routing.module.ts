8836121d558651d30e2b8d5453f35634bf46e16b
MichaelOdera <michogelira@gmail.com>
2020-01-21 16:02:19 +0300

    add type any to repositories

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProfileComponent } from './profile/profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { RepositoriesComponent} from './repositories/repositories.component';
 
 
 const routes: Routes = [
   {path:'', component:ProfileComponent},
   {path:'profile', component:ProfileComponent},
+  {path:'repos', component:RepositoriesComponent},
   {path:'**', component:NotFoundComponent},
   {path: '', redirectTo:"/profile", pathMatch:"full"},
 ];
